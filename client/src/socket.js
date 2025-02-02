57f3e438da1bc8e0404fb2ec27f92431d34ffd09
Arshad-ashuu <mohammadarshad01474.com>
2025-02-02 17:42:45 +0530

    fix dashoard scores

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,4 @@
 // src/socket.js
 import { io } from 'socket.io-client';
-const socket = io('https://stakes-8pio.onrender.com'); // Adjust URL/port as needed.
+const socket = io('https://stakes-ft.onrender.com'); // Adjust URL/port as needed.
 export default socket;
