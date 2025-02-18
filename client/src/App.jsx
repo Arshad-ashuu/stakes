044448d2fc5c1eff22b72e8bc0eb8648af5bee79
Arshad-ashuu <mohammadarshad01474.com>
2025-02-18 22:30:29 +0530

    fix delay in sockets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <div className="app-container">
         {/* <h1 className="title"><span className="t1">R</span><span className="t2">i</span><span className="t3">s</span><span className="t4">k</span><span> </span><span className="t5">I</span><span className="t6">t</span><span> </span><span className="t1">R</span><span className="t2">i</span><span className="t3">g</span><span className="t1">h</span><span className="t2">t</span></h1> */}
         <h1 className='title-sub'>TECHNOPHILIA <span className='text-minsub'>4.0</span></h1>
-        <h1 className='title'>Risk it Right<img src='src\assets\emoji-mashup.png' alt='Technophilia-Logo' style={{width:'40px',height:'34px',marginLeft:'6px'}} /></h1>
+        <h1 className='title'>Risk it Right<img src='./emoji-mashup.png' alt='' style={{width:'40px',height:'34px',marginLeft:'6px'}} /></h1>
         <CreateRoom setRoomCode={setRoomCode} setPlayerName={setPlayerName} setIsHost={setIsHost} />
         <hr className="divider" />
         <JoinRoom setRoomCode={setRoomCode} setPlayerName={setPlayerName} />
