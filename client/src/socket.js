ec7a943f0d7b64df039de579a82bf660db0ab2f9
Arshad-ashuu <mohammadarshad01474.com>
2025-02-22 11:14:59 +0530

    socket delay

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,19 @@
 // src/socket.js
 import { io } from 'socket.io-client';
-const socket = io('https://stakes-ft.onrender.com');
-//  const socket = io('http://localhost:5000'); // Adjust URL/port as needed.
+const socket = io('https://stakes-ft.onrender.com', {
+  transports: ['websocket'], // Ensure WebSocket transport is prioritized
+  reconnection: true,        // Enable automatic reconnection
+  reconnectionAttempts: 5,    // Try reconnecting 5 times
+  reconnectionDelay: 2000,    // Wait 2 seconds before retrying
+  reconnectionDelayMax: 10000 // Max delay for retries
+});
+
+//   const socket = io('http://localhost:5000', {
+//     transports: ['websocket'], // Ensure WebSocket transport is prioritized
+//     reconnection: true,        // Enable automatic reconnection
+//     reconnectionAttempts: 5,    // Try reconnecting 5 times
+//     reconnectionDelay: 2000,    // Wait 2 seconds before retrying
+//     reconnectionDelayMax: 10000 // Max delay for retries
+//   });
+   // Adjust URL/port as needed.
 export default socket;
